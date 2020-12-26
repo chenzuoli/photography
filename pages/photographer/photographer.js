12df9aef374116607e214e98d370a819a4f69c65
chenzuoli <chenzuoli709@163.com>
2020-12-26 23:28:04 +0800

    添加【看一看】主页

--- pages/photographer/photographer.js
+++ pages/photographer/photographer.js
@@ -1,4 +1,7 @@
 // pages/photographer/photographer.js
+const app = getApp();
+var get_photographies_url = "https://localhost:7449/photography/get_photographies"
+
 Component({
   /**
    * Component properties
@@ -11,13 +14,59 @@ Component({
    * Component initial data
    */
   data: {
-
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    photographies: [],
+    isShow: true
   },
-
+  ready: function() {
+    var that = this
+    let token = wx.getStorageSync("token");
+    wx.request({
+      url: get_photographies_url,
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "token": token
+      },
+      method: 'get',
+      dataType: 'json',
+      responseType: 'text',
+      complete: (res) => {},
+      fail: (res) => {
+        console.log("获取作品列表失败。")
+        wx.showToast({
+          title: '获取失败',
+          icon: "warn",
+          duration: 1000
+        })
+      },
+      success: (result) => {
+        console.log(result.data)
+        if (result.data.data.length > 0) {
+          that.setData({
+            isShow: false
+          })
+        }
+        that.setData({
+          "photographies": result.data.data
+        })
+      }
+    })
+  },
   /**
    * Component methods
    */
   methods: {
-
+    isCard(e) {
+      this.setData({
+        isCard: e.detail.value
+      })
+    },
+    add(e){
+      wx.navigateTo({
+        url: '../photography/photography',
+      })
+    }
+    
   }
 })
